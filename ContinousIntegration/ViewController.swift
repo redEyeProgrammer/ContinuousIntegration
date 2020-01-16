bc2adc7a650c91ba18d4d185fc020028a6afed51
Oge Nwabuoku <oge.nwabuoku@infor.com>
2020-01-16 11:19:16 -0500

    First Commit

---
+++ ContinousIntegration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContinousIntegration
+//
+//  Created by Oge Nwabuoku on 1/16/20.
+//  Copyright © 2020 Oge Nwabuoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
